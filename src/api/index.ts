6090d99ae60e3c9bda343fed5180ffde09dbce2c
ethan6077 <ethan6077@gmail.com>
2019-06-11 13:38:59 +1000

    api fetchPropertiesList done

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,14 @@
-export function fetchPropertiesList(): Promise<any> {
-  return fetch('https://code-challenge.activepipe.com/challenge/properties');
+import { PropertyI } from '../types';
+
+export async function fetchPropertiesList(): Promise<PropertyI[]> {
+  try {
+    const response = await fetch(
+      'https://code-challenge.activepipe.com/challenge/properties'
+    );
+    const dataInJson = await response.json();
+    return dataInJson;
+  } catch (e) {
+    console.warn(e);
+    return [];
+  }
 }

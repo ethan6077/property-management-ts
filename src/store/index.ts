d0af4a023aaac7089227e7ac018eb1122eea5207
ethan6077 <ethan6077@gmail.com>
2019-06-21 12:13:44 +1000

    added redux-devtools-extension

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { propertiesReducer } from '../reducers/index';
 
-const store = createStore(propertiesReducer, applyMiddleware(thunk));
+const store = createStore(
+  propertiesReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;

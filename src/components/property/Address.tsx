f7f39b938e2e8c2e36679062f619f1c2b08b94fc
Ethan Zhang <ethan6077@gmail.com>
2021-01-16 18:46:13 +1100

    feat: Address upgraded

--- src/components/property/Address.tsx
+++ src/components/property/Address.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import styles from './Address.module.css';
-import { AddressI } from '../../types';
+import React, { FC } from "react";
+import styles from "./Address.module.css";
+import { AddressI } from "../../types";
 
-function Address(props: AddressI): JSX.Element {
-  const { street, suburb, state, postcode } = props;
-  return (
-    <div className={styles.container}>
-      <span>{street}</span>
-      <span>{suburb}</span>
-      <span>{state + ' ' + postcode}</span>
-    </div>
-  );
-}
+const Address: FC<AddressI> = ({ street, suburb, state, postcode }) => (
+  <div className={styles.container}>
+    <span>{street}</span>
+    <span>{suburb}</span>
+    <span>{state + " " + postcode}</span>
+  </div>
+);
 
 export default Address;

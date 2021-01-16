3637b8ee9bad1ee0763ee3b423e609849851bc38
Ethan Zhang <ethan6077@gmail.com>
2021-01-16 18:28:27 +1100

    feat: component SmallCirle upgraded

--- src/components/property/SmallCircle.tsx
+++ src/components/property/SmallCircle.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import classNames from 'classnames';
-import styles from './SmallCircle.module.css';
-import { PropertyStatusE } from '../../types';
+import React, { FC } from "react";
+import classNames from "classnames";
+import styles from "./SmallCircle.module.css";
+import { PropertyStatusE } from "../../types";
 
-interface Props {
+interface SmallCircleProps {
   status: PropertyStatusE;
 }
 
-function SmallCircle(props: Props): JSX.Element {
-  const { status } = props;
-  return (
-    <span
-      className={classNames({
-        [styles.current]: status === PropertyStatusE.Current,
-        [styles.sold]: status === PropertyStatusE.Sold,
-        [styles.offMarket]: status === PropertyStatusE.OffMarket,
-        [styles.withDrawn]: status === PropertyStatusE.WithDrawn
-      })}
-    ></span>
-  );
-}
+const SmallCircle: FC<SmallCircleProps> = ({ status }) => (
+  <span
+    className={classNames({
+      [styles.current]: status === PropertyStatusE.Current,
+      [styles.sold]: status === PropertyStatusE.Sold,
+      [styles.offMarket]: status === PropertyStatusE.OffMarket,
+      [styles.withDrawn]: status === PropertyStatusE.WithDrawn
+    })}
+  ></span>
+);
 
 export default SmallCircle;

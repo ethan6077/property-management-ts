f97c0622d2de3439d8690f40c49483d4582c1087
Ethan Zhang <ethan6077@gmail.com>
2021-01-16 18:49:08 +1100

    Property upgraded

--- src/components/property/Property.tsx
+++ src/components/property/Property.tsx
@@ -1,35 +1,32 @@
-import React from 'react';
-import styles from './Property.module.css';
-import Price from './Price';
-import PropertyStatus from './PropertyStatus';
-import Address from './Address';
-import { PropertyI } from '../../types';
+import React, { FC } from "react";
+import styles from "./Property.module.css";
+import Price from "./Price";
+import PropertyStatus from "./PropertyStatus";
+import Address from "./Address";
+import { PropertyI } from "../../types";
 
-interface Props {
+interface PropertyProps {
   property: PropertyI;
 }
 
-function Property(props: Props): JSX.Element {
-  const { property } = props;
-  return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <Price price={property.price} />
-        <PropertyStatus status={property.status} />
-      </div>
-      <div className={styles.imgContainer}>
-        <img src={property.image} alt={'property_img'} />
-      </div>
-      <div>
-        <Address
-          street={property.street}
-          suburb={property.suburb}
-          state={property.state}
-          postcode={property.postcode}
-        />
-      </div>
+const Property: FC<PropertyProps> = ({ property }) => (
+  <div className={styles.container}>
+    <div className={styles.header}>
+      <Price price={property.price} />
+      <PropertyStatus status={property.status} />
     </div>
-  );
-}
+    <div className={styles.imgContainer}>
+      <img src={property.image} alt={"property_img"} />
+    </div>
+    <div>
+      <Address
+        street={property.street}
+        suburb={property.suburb}
+        state={property.state}
+        postcode={property.postcode}
+      />
+    </div>
+  </div>
+);
 
 export default Property;
